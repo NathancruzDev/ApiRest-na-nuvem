558b6810ff64cdad2dde70e962656bbe393187f5
Nathan <nathanleitedacruz@gmail.com>
2025-05-05 14:11:19 -0300

    add OpenApiDefinition on main

--- demo/src/test/java/edu/dio/DemoApplicationTests.java
+++ demo/src/test/java/edu/dio/DemoApplicationTests.java
@@ -1,8 +1,10 @@
 package edu.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@OpenAPIDefinition(servers = {@Server(url="/",description = "Default Server Url")})
 @SpringBootTest
 class DemoApplicationTests {
 
